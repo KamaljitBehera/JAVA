86beddffdcd444f65da3e0fc7eda33aa79e1d464
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-11-30 23:44:13 +0530

    first program added successfully

---
+++ core_Java/src/com/org/basics/FirstProgram.java
@@ -0,0 +1,7 @@
+package com.org.basics;
+//This is the first program to print simple statement using java
+public class FirstProgram {
+  public static void main(String[] args) {
+	System.out.println("Hello World!");
+}
+}
